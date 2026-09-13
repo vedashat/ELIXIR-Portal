5aa2110c8c01aadb3cbaa2bfc8989867044a9679
Vedashat <akshatvedant@gmail.com>
2026-09-13 17:56:24 +0530

    Fixed syntax error

--- backend/db/db.js
+++ backend/db/db.js
@@ -6,7 +6,7 @@ dotenv.config()
 const { Pool } = pg
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 })
 
